038a7e5b6ae29d9747be0e969be41d380332276c
Omar Faruqe <faruqe.cse@gmail.com>
2016-01-16 17:06:46 -0500

    Initial Commit

---
+++ MapMyPoints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapMyPoints
+//
+//  Created by Omar Faruqe on 2016-01-16.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapMyPoints/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapMyPoints
+//
+//  Created by Omar Faruqe on 2016-01-16.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
